24358ed554efe4dcec8107015af1d2546bf7b11f
Devansh <devanshbhargava90@gmail.com>
2024-11-08 11:38:35 +0530

    constructor Book added and function to add Book in the array also added

---
+++ script.js
@@ -0,0 +1,15 @@
+const myLibrary = [];
+
+
+function Book (title,author,pages,readOrNot){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readOrNot = readOrNot;
+}
+
+function addBookToLibrary(title,author,pages,readOrNot) {
+    newBook = new Book(title,author,pages,readOrNot);
+    myLibrary.push(newBook);
+
+}
