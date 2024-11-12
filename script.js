538adfae5157e24ad2124851efb453e55ce7c50b
Devansh <devanshbhargava90@gmail.com>
2024-11-12 23:30:15 +0530

    function to iterate added and form html code added

--- script.js
+++ script.js
@@ -1,21 +1,66 @@
-const myLibrary = [];
+document.addEventListener('DOMContentLoaded', function(){
+    const myLibrary = [];
 
 
-function Book (title,author,pages,readOrNot){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.readOrNot = readOrNot;
-}
+    function Book (title,author,pages,readOrNot){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.readOrNot = readOrNot;
+    }
 
-function addBookToLibrary(title,author,pages,readOrNot) {
-    newBook = new Book(title,author,pages,readOrNot);
-    myLibrary.push(newBook);
+    function addBookToLibrary(title,author,pages,readOrNot) {
+        newBook = new Book(title,author,pages,readOrNot);
+        myLibrary.push(newBook);
 
-}
+    }
 
 
-// function createCard(book){
-//     const cardContainer = document.createElement('div');
-//
-// }
+    function createCard(book){
+        const booksContainer = document.querySelector('.bookCardsContainer');
+        const cardContainer = document.createElement('div');
+        cardContainer.setAttribute('class','cardContainer');
+
+        //creating the child elements of cardConatainer
+
+        const titleContainer = document.createElement('div');
+        titleContainer.setAttribute('class','title');
+        titleContainer.textContent = book.title;
+        const dividingLine = document.createElement('hr');
+
+        const detailsContainer = document.createElement('div');
+        detailsContainer.setAttribute('class','details');
+
+        //creating elements which are inside the details
+        const listContainer = document.createElement('ul');
+        //creating li elements which are into ul
+        const list1 = document.createElement('li');
+        list1.textContent = book.author;
+        const list2 = document.createElement('li');
+        list2.textContent = book.pages;
+        const list3 = document.createElement('li');
+        list3.className = 'status';
+        list3.textContent = book.readOrNot;
+
+        listContainer.append(list1,list2,list3);
+
+        detailsContainer.appendChild(listContainer);
+
+        cardContainer.append(titleContainer,dividingLine,detailsContainer);
+
+        booksContainer.appendChild(cardContainer);
+
+    }
+
+    function iterateLibrary(library){
+        library.forEach(function (item,index){
+            createCard(item);
+        })
+    }
+
+    addBookToLibrary("The Hobbit","j.k rowling","350","not read");
+    addBookToLibrary("immortals of meluha","amish","400","read");
+    addBookToLibrary("miles to go","devansh","100","not read");
+    iterateLibrary(myLibrary);
+
+})
